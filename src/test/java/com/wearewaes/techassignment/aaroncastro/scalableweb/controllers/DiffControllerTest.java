84f8f40796853b5f84bf4b7de651e6bab0754e7d
Aaron Castro <aacastro@backcountry.com>
2019-10-14 06:46:42 -0600

    Add integration test for right and left endpoints

--- src/test/java/com/wearewaes/techassignment/aaroncastro/scalableweb/controllers/DiffControllerTest.java
+++ src/test/java/com/wearewaes/techassignment/aaroncastro/scalableweb/controllers/DiffControllerTest.java
@@ -5,8 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -17,33 +25,132 @@ public class DiffControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    public void insertLeftWithEmptyBodyTest() {
+    public void insertLeftWithEmptyBodyTest() throws Exception {
+        final String body = " ";
 
+        mockMvc.perform(
+                    post("/v1/diff/{id}/left", "test-01")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .contentType(MediaType.TEXT_PLAIN)
+                    .content(body)
+        )
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 
     @Test
-    public void insertLeftWithNotBase64BodyTest() {
+    public void insertLeftWithNotBase64BodyTest() throws Exception {
+        final String body = "%^$";
 
+        mockMvc.perform(
+                post("/v1/diff/{id}/left", "test-01")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.TEXT_PLAIN)
+                        .content(body)
+        )
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 
     @Test
-    public void insertLeftWithInvalidJSONBodyTest() {
+    public void insertLeftWithValidJSONBodyTest() throws Exception {
+        final String body = "eyJtZXNzYWdlIjogIkhpIGZyb20gQ29zdGEgUmljYSEifQ==";
 
+        mockMvc.perform(
+                post("/v1/diff/{id}/left", "test-01")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.TEXT_PLAIN)
+                        .content(body)
+        )
+                .andDo(print())
+                .andExpect(status().isCreated());
     }
 
     @Test
-    public void insertRightWithEmptyBodyTest() {
+    public void insertLeftWithDuplicatedJSONBodyTest() throws Exception {
+        final String body = "eyJtZXNzYWdlIjogIkhpIGZyb20gQ29zdGEgUmljYSEifQ==";
 
+        mockMvc.perform(
+                post("/v1/diff/{id}/left", "duplicated-01")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.TEXT_PLAIN)
+                        .content(body)
+        )
+                .andDo(print())
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(
+                post("/v1/diff/{id}/left", "duplicated-01")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.TEXT_PLAIN)
+                        .content(body)
+        )
+                .andDo(print())
+                .andExpect(status().isConflict());
     }
 
     @Test
-    public void insertRightWithNotBase64BodyTest() {
+    public void insertRightWithEmptyBodyTest() throws Exception {
+        final String body = " ";
 
+        mockMvc.perform(
+                post("/v1/diff/{id}/right", "test-01")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.TEXT_PLAIN)
+                        .content(body)
+        )
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 
     @Test
-    public void insertRightWithInvalidJSONBodyTest() {
+    public void insertRightWithNotBase64BodyTest() throws Exception {
+        final String body = "*&^%";
 
+        mockMvc.perform(
+                post("/v1/diff/{id}/right", "test-01")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.TEXT_PLAIN)
+                        .content(body)
+        )
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 
+    @Test
+    public void insertRightWithValidJSONBodyTest() throws Exception {
+        final String body = "eyJtZXNzYWdlIjogIkhpIGZyb20gQ29zdGEgUmljYSEifQ==";
+
+        mockMvc.perform(
+                post("/v1/diff/{id}/right", "test-01")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.TEXT_PLAIN)
+                        .content(body)
+        )
+                .andDo(print())
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void insertRightWithDuplicatedJSONBodyTest() throws Exception {
+        final String body = "eyJtZXNzYWdlIjogIkhpIGZyb20gQ29zdGEgUmljYSEifQ==";
+
+        mockMvc.perform(
+                post("/v1/diff/{id}/right", "duplicated-01")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.TEXT_PLAIN)
+                        .content(body)
+        )
+                .andDo(print())
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(
+                post("/v1/diff/{id}/right", "duplicated-01")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.TEXT_PLAIN)
+                        .content(body)
+        )
+                .andDo(print())
+                .andExpect(status().isConflict());
+    }
 }
